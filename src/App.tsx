2a5a175110940b3f23401656a7209642d6cfe977
ryoya <sateraito3696@gmail.com>
2025-05-27 12:58:33 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useSplitter } from "./hooks/useSplitter";
 
 function App() {
-  const { explorerWidth, handleExplorerMouseDown, handleDragable} = useSplitter()
+  const { explorerWidth, handleExplorerMouseDown, handleDragable } = useSplitter();
 
   return (
     // 全体
@@ -11,15 +11,19 @@ function App() {
       <div className="w-[40px] h-full border-l-1"></div>
 
       {/* エクスプローラー */}
-      <div className={"h-full relative border-l-1"} style={{width: `${explorerWidth}px`}}>
+      <div className={"h-full relative border-l-1"} style={{ width: `${explorerWidth}px` }}>
         {/* splitter */}
-        <div className="h-full absolute right-0 w-[5px] hover:bg-blue-300" onMouseDown={handleExplorerMouseDown} onDrag={handleDragable}/>
+        <div
+          className="h-full absolute right-0 w-[5px] hover:bg-blue-300"
+          onMouseDown={handleExplorerMouseDown}
+          onDrag={handleDragable}
+        />
       </div>
 
       {/* エディター（タブ） */}
       <div className="flex-1 h-full border-l-1"></div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/hooks/useSplitter.ts
+++ src/hooks/useSplitter.ts
@@ -1,32 +1,34 @@
-import { useCallback, useState } from "react"
+import { useCallback, useState } from "react";
 
 type useSplitterReturns = {
-    explorerWidth: number,
-    handleExplorerMouseDown: (event: React.MouseEvent) => void,
-    handleDragable: (event:React.MouseEvent) => void
-}
+  explorerWidth: number;
+  handleExplorerMouseDown: (event: React.MouseEvent) => void;
+  handleDragable: (event: React.MouseEvent) => void;
+};
 
 export function useSplitter(): useSplitterReturns {
-    const [explorerWidth, setExplorerWidth] = useState<number>(300)
+  const [explorerWidth, setExplorerWidth] = useState<number>(300);
 
-    const handleDragable = useCallback((event:React.MouseEvent) => {event.preventDefault()}, [])
+  const handleDragable = useCallback((event: React.MouseEvent) => {
+    event.preventDefault();
+  }, []);
 
-    const handleExplorerMouseDown = useCallback((event: React.MouseEvent) => {
-        event.preventDefault()
-        document.addEventListener('mousemove', handleExplorerMouseMove)
-        document.addEventListener('mouseup', handleExplorerMouseUp)
-    }, [])
+  const handleExplorerMouseDown = useCallback((event: React.MouseEvent) => {
+    event.preventDefault();
+    document.addEventListener("mousemove", handleExplorerMouseMove);
+    document.addEventListener("mouseup", handleExplorerMouseUp);
+  }, []);
 
-    const handleExplorerMouseMove = useCallback((event: MouseEvent) => {
-        event.preventDefault()
-        setExplorerWidth(pre => pre + event.movementX)
-    }, [])
+  const handleExplorerMouseMove = useCallback((event: MouseEvent) => {
+    event.preventDefault();
+    setExplorerWidth((pre) => pre + event.movementX);
+  }, []);
 
-    const handleExplorerMouseUp = useCallback((event: MouseEvent) => {
-        event.preventDefault()
-        document.removeEventListener('mousemove', handleExplorerMouseMove)
-        document.removeEventListener('mouseup', handleExplorerMouseUp)
-    }, [])
+  const handleExplorerMouseUp = useCallback((event: MouseEvent) => {
+    event.preventDefault();
+    document.removeEventListener("mousemove", handleExplorerMouseMove);
+    document.removeEventListener("mouseup", handleExplorerMouseUp);
+  }, []);
 
-    return { explorerWidth, handleExplorerMouseDown, handleDragable }
+  return { explorerWidth, handleExplorerMouseDown, handleDragable };
 }
