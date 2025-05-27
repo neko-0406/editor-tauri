e460e991327b1efe3ed4621e39da0000e217645c
ryoya <sateraito3696@gmail.com>
2025-05-27 12:50:03 +0900

    splitter用のカスタムフック作成、 位置の調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import "./App.css";
+import { useSplitter } from "./hooks/useSplitter";
 
 function App() {
-  const [explorerWidth, setExplorerWidth] = useState<number>(300)
+  const { explorerWidth, handleExplorerMouseDown, handleDragable} = useSplitter()
 
   return (
     // 全体
@@ -11,7 +11,10 @@ function App() {
       <div className="w-[40px] h-full border-l-1"></div>
 
       {/* エクスプローラー */}
-      <div className={"h-full border-l-1"} style={{width: `${explorerWidth}px`}}></div>
+      <div className={"h-full relative border-l-1"} style={{width: `${explorerWidth}px`}}>
+        {/* splitter */}
+        <div className="h-full absolute right-0 w-[5px] hover:bg-blue-300" onMouseDown={handleExplorerMouseDown} onDrag={handleDragable}/>
+      </div>
 
       {/* エディター（タブ） */}
       <div className="flex-1 h-full border-l-1"></div>
---
+++ src/hooks/useSplitter.ts
@@ -0,0 +1,32 @@
+import { useCallback, useState } from "react"
+
+type useSplitterReturns = {
+    explorerWidth: number,
+    handleExplorerMouseDown: (event: React.MouseEvent) => void,
+    handleDragable: (event:React.MouseEvent) => void
+}
+
+export function useSplitter(): useSplitterReturns {
+    const [explorerWidth, setExplorerWidth] = useState<number>(300)
+
+    const handleDragable = useCallback((event:React.MouseEvent) => {event.preventDefault()}, [])
+
+    const handleExplorerMouseDown = useCallback((event: React.MouseEvent) => {
+        event.preventDefault()
+        document.addEventListener('mousemove', handleExplorerMouseMove)
+        document.addEventListener('mouseup', handleExplorerMouseUp)
+    }, [])
+
+    const handleExplorerMouseMove = useCallback((event: MouseEvent) => {
+        event.preventDefault()
+        setExplorerWidth(pre => pre + event.movementX)
+    }, [])
+
+    const handleExplorerMouseUp = useCallback((event: MouseEvent) => {
+        event.preventDefault()
+        document.removeEventListener('mousemove', handleExplorerMouseMove)
+        document.removeEventListener('mouseup', handleExplorerMouseUp)
+    }, [])
+
+    return { explorerWidth, handleExplorerMouseDown, handleDragable }
+}
