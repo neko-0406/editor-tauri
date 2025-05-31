222577e6ad47a7c23df79b9299b252c5f7020dc4
neko-04 <sateraito3696@gmail.com>
2025-06-01 00:21:21 +0900

    lexicalとエディターの追加

---
+++ src/components/editor/Editor.tsx
@@ -0,0 +1,61 @@
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { CheckListPlugin } from "@lexical/react/LexicalCheckListPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { SelectionAlwaysOnDisplay } from "@lexical/react/LexicalSelectionAlwaysOnDisplay";
+import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
+import { TablePlugin } from "@lexical/react/LexicalTablePlugin";
+import { useEffect } from "react";
+
+import { checkDir, EditorOnChangePlugin, onChange } from "../../hooks/EditorOnChangePlugin";
+import theme from "./editorTheme";
+import nodes from "./nodes";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function onError(error: any) {
+  console.error(error);
+}
+
+export default function Editor() {
+  const initialConfig = {
+    namespace: "tauri-editor",
+    theme,
+    onError,
+    nodes: nodes,
+  };
+
+  useEffect(() => {
+    checkDir();
+  }, []);
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={
+          <ContentEditable
+            className="focus-visible:outline-none"
+            aria-placeholder="enter some text..."
+            placeholder={<div>enter some text...</div>}
+          />
+        }
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+      <LinkPlugin />
+      <ListPlugin />
+      <CheckListPlugin />
+      <TablePlugin />
+      <TabIndentationPlugin maxIndent={10} />
+      <MarkdownShortcutPlugin />
+      <SelectionAlwaysOnDisplay />
+      <EditorOnChangePlugin onChange={onChange} />
+    </LexicalComposer>
+  );
+}
---
+++ src/hooks/EditorOnChangePlugin.tsx
@@ -0,0 +1,43 @@
+import { $convertToMarkdownString, TRANSFORMERS } from "@lexical/markdown";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { BaseDirectory, exists, mkdir, writeTextFile } from "@tauri-apps/plugin-fs";
+import { EditorState } from "lexical";
+import { useEffect } from "react";
+
+type EditorOnChangeProps = {
+  onChange: (state: EditorState) => void;
+};
+
+export function EditorOnChangePlugin({ onChange }: EditorOnChangeProps) {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      onChange(editorState);
+    });
+  }, [editor, onChange]);
+
+  return null;
+}
+
+export function onChange(editorState: EditorState) {
+  editorState.read(async () => {
+    try {
+      const baseDir = BaseDirectory.AppData;
+      const markdown = $convertToMarkdownString(TRANSFORMERS);
+      await writeTextFile("editor.md", markdown, { baseDir: baseDir });
+      console.log("File saved!");
+    } catch (error) {
+      console.error("Error", error);
+    }
+  });
+}
+
+export async function checkDir() {
+  const baseDir = BaseDirectory.AppData;
+  const appDataExists = await exists(".", { baseDir: baseDir });
+  if (!appDataExists) {
+    await mkdir(".", { baseDir: baseDir, recursive: true });
+    console.log("Directory created!");
+  }
+}
