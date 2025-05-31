1f4abdd0828a8d7653f2ab688c8d5a62297481a5
neko-04 <sateraito3696@gmail.com>
2025-05-31 14:12:53 +0900

    並び替え実装途中

--- src/components/EditorTab.tsx
+++ src/components/EditorTab.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 import { FaCircleXmark } from "react-icons/fa6";
 
 export type TabItemData = {
@@ -31,6 +31,7 @@ const data: TabItemData[] = [
 ];
 
 export function TabContainer() {
+  const [tabDatalist, setTabDatalist] = useState<TabItemData[] | null>(data);
   const [selectedTabId, setSelectedTabId] = useState<string | null>(null);
 
   useEffect(() => {
@@ -40,14 +41,20 @@ export function TabContainer() {
   return (
     <div className="w-full h-hull flex flex-col">
       {/* タブ上部のタグ表示部分 */}
-      <div className="w-full h-[40px] flex flex-row bg-gray-100">
-        {data?.map((item) => (
-          <TabItemTag item={item} selectedTabId={selectedTabId} setSelectedTabId={setSelectedTabId} />
-        ))}
+      <div className="w-full h-[40px] flex flex-row bg-gray-300">
+        {tabDatalist
+          ? tabDatalist.map((item) => (
+              <TabItemTag item={item} selectedTabId={selectedTabId} setSelectedTabId={setSelectedTabId} />
+            ))
+          : null}
       </div>
       {/* エディター部分 */}
       <div className="w-full h-100 top-40">
-        {selectedTabId ? data?.find((item) => item.id === selectedTabId)?.editorComponent : null}
+        {selectedTabId
+          ? tabDatalist
+            ? tabDatalist.find((item) => item.id === selectedTabId)?.editorComponent
+            : null
+          : null}
       </div>
     </div>
   );
@@ -60,12 +67,26 @@ export type TabItemTagProps = {
 };
 
 export function TabItemTag({ item, selectedTabId, setSelectedTabId }: TabItemTagProps) {
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const handleDragStart = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    event.dataTransfer.setData("text/plain", item.id);
+  }, []);
+
+  const handleDragEnd = useCallback(() => {
+    setIsDragging(false);
+  }, []);
+
   return (
     <div
       key={item.id}
       onClick={() => setSelectedTabId(item.id)}
-      className="mr-[1px] ml-[1px] px-2 cursor-pointer bg-gray-100 "
-      style={{ backgroundColor: item.id === selectedTabId ? "white" : "#f3f4f6" }}
+      draggable={true}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      className="mr-[1px] ml-[1px] px-2 cursor-pointer bg-gray-200"
+      style={{ backgroundColor: item.id === selectedTabId ? "white" : "#f3f4f6", opacity: isDragging ? "0.5" : "1.0" }}
     >
       {item.name}
       <button type="button">
