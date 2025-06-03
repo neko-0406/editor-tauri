5917151fe9d122beb840f62c1820d5584a0cc47b
ryoya <sateraito3696@gmail.com>
2025-06-03 15:36:30 +0900

    関数作成

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,19 @@
+use std::path::Path;
+
+mod storage;
+use storage::file_manager::{walk_tree, FileItem};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![get_file_tree])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_file_tree(path: String) -> Option<FileItem> {
+    let path = Path::new(&path);
+    return walk_tree(path);
 }
