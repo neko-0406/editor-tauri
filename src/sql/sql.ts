65f8644e156890cf0f64c0e04dbf3a5a69d5a906
Ryoya Otomo <s2212214@st.tohtech.ac.jp>
2025-06-06 13:15:53 +0900

    途中

--- src/sql/sql.ts
+++ src/sql/sql.ts
@@ -1,31 +1,36 @@
+import { path } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/core";
 import Database from "@tauri-apps/plugin-sql";
 
 import { SettingType } from "../components/setting/SettingProvider";
 
-const db = await Database.load("sqlite:setting.db");
+class AppConfigDB {
+  private dataBasePath: string | null = null;
+  private dataBase: Database | null = null;
 
-export async function createSettingTable() {
-  try {
-    const CREATE_TABLE_QUERY = `
-      CREATE TABLE IF NOT EXISTS setting (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        setting TEXT NOT NULL
+  // コンストラクタ
+  constructor() {
+    // 即時実行関数で初期化
+    (async () => {
+      const exefileDir = await this.getExeFileDir();
+      this.dataBasePath = await path.join(exefileDir, "setting.db");
+      this.dataBase = await Database.load("sqlite:" + this.dataBasePath);
+    })();
+  }
+
+  // 実行ファイルのあるdir取得
+  private async getExeFileDir(): Promise<string> {
+    return await invoke("get_current_exe_dir");
+  }
+
+  // テーブルの定義と初期化
+  private createSettingTable() {
+    const query = `
+      create table if not exists setting (
+        key text primary key,
+        value text,
+        type text
       )
     `;
-    await db.execute(CREATE_TABLE_QUERY);
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
-}
-
-export async function executeSettingQuery(params: SettingType[] = []) {
-  try {
-    const SETTING_QUERY = "INSERT into setting (setting) VALUES ($1)";
-    const result = await db.execute(SETTING_QUERY, params);
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
   }
 }
